a9a39da823ae919c189cf9306eec58b8a63d74fd
Typer1079 <Typer1079@gmail.com>
2024-12-01 23:31:05 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,12 @@
 // service-worker.js
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('static-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/2ndScreen.css',
-        '/2ndScreen.js',
-        // Add other necessary files like images, etc.
-      ]);
-    })
-  );
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated.');
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('Service Worker fetching:', event.request.url);
 });
